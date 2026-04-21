0ab4759cd323aa2a1a15e0ae8ba7789195be8dae
lakshithagnk <166338846+lakshithagnk@users.noreply.github.com>
2026-04-22 01:07:18 +0530

    11

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
 
-  base: "/engi-glow", 
+    base: "/engi-glow/",
 
   server: {
     host: "::",
